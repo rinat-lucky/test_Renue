ee8f51523abfc9e40e6b72a95473d50331cb05cf
rinat_k <rinat1kam@mail.ru>
2023-01-14 21:41:39 +0500

    init commit

---
+++ src/components/app/App.js
@@ -0,0 +1,17 @@
+import logo from './renue-logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="renue_logo" />
+        <p>
+          test_Renue by Rinat_K
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
